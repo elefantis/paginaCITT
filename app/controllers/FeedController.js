d8dc4874711c6c769f6c967da1a961372e068ca5
Ignacio Donoso <naxonicolas1997@gmail.com>
2019-05-06 19:25:33 -0400

    Utilizando json
    Se agrego el json de la pagina de inicio con los talleres que se
    imparte.
    
    Se modifico los controllers para que mostraran las listas de los jsons
    en la pagina de subjects y feed

--- app/controllers/FeedController.js
+++ app/controllers/FeedController.js
@@ -1,8 +1,43 @@
 function FeedController() {
-    // Llama.get("../app/data/feed.json").then( ( data ) => {
-    //     console.log( data[0] );
-    // } ).catch( ( error ) => {
-    //     console.log( error );
-    // });
+    Llama.get('../app/data/feed.json').then( ( data ) => {
+        let container = document.getElementById('news-list');
+
+        for(let i in data)
+        {
+            //Crear elementos
+            let news = document.createElement('div');
+            let title = document.createElement('h1');
+            let paragraph = document.createElement('p');
+            let video = document.createElement('div');
+            let image = document.createElement('img');
+
+            //Ingresar datos
+            title.innerHTML = data[i].name;
+            paragraph.innerHTML = data[i].data;
+            video.innerHTML = data[i].video;
+            image.src = data[i].image;
+
+            //Atributos
+            title.className = 'news-title';
+            image.className = 'news-image';
+            image.alt = data[i].name;
+            paragraph.className = 'news-paragraph';
+
+            //Insertar los elementos al div
+            news.appendChild(title);
+            news.appendChild(paragraph);
+            if(data[i].video !== '')
+            {
+                news.appendChild(video);
+            }
+            if(data[i].image !== '')
+            {
+                news.appendChild(image);
+            }
+            container.appendChild(news);
+        }
+    } ).catch( ( error ) => {
+        console.log( error );
+    });
     
-}
+};
--- app/controllers/SubjectsController.js
+++ app/controllers/SubjectsController.js
@@ -1,3 +1,41 @@
 function SubjectsController () {
+    Llama.get('../app/data/subject.json').then( ( data ) => {
+        let container = document.getElementById('workshop');
 
-}
+        for(let i in data)
+        {
+            //Crear elementos
+            let workshop = document.createElement('div');
+            let title = document.createElement('h1');
+            let room = document.createElement('p');
+            let day = document.createElement('p');
+            let hour = document.createElement('p');
+
+            //ingresar datos
+            title.innerHTML = data[i].name;
+            room.innerHTML = data[i].place;
+            day.innerHTML = data[i].day;
+            hour.innerHTML = data[i].hour;
+
+            //Atributos
+            title.className = 'subjects-title';
+            room.className = 'subjects-room';
+            day.className = 'subjects-day';
+            hour.className = 'subjects-hour';
+            
+            //Insertar elementos al div
+            workshop.appendChild(title);
+            workshop.appendChild(room);
+            workshop.appendChild(day);
+            workshop.appendChild(hour);
+
+            container.appendChild(workshop);
+        }
+    }).catch( ( error ) => 
+    {
+        console.log( error );
+    });
+
+
+
+};
