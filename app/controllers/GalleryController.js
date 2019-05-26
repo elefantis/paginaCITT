257061e3bb57000abd309a139a1e33c74fa1a4be
Ignacio Donoso <naxonicolas1997@gmail.com>
2019-05-25 22:45:58 -0400

    Galery with llama.js

--- app/controllers/GalleryController.js
+++ app/controllers/GalleryController.js
@@ -1,3 +1,24 @@
 function GalleryController() {
-
+    Llama.get('../app/data/gallery.json').then( ( data ) => {
+        let container = document.getElementById('general-container');
+        container.innerHTML = '';
+        data.forEach( d => {
+            let showImages = '';
+            d.images.forEach( imageSrc => {
+                console.log(imageSrc);
+                showImages += 
+                `<a class="item-img" href="${imageSrc}" data-lightbox="mygallery-hackaton"><img src="${imageSrc}" alt="Aviones" /></a>`;
+            });
+            container.innerHTML += 
+            `<div class="galery-container">
+                <div class="galery-separator">
+                    <h2>${d.title}</h2>
+                </div>
+                ${showImages}
+            </div>`;
+        });
+    }).catch( ( error ) => 
+    {
+        console.log( error );
+    });
 };
