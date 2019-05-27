2adab1e7047e980bf345cc13610fa42f6f966c1b
Ignacio Donoso <naxonicolas1997@gmail.com>
2019-05-27 14:17:36 -0400

    Merge

--- app/controllers/TeamController.js
+++ app/controllers/TeamController.js
@@ -1,3 +1,66 @@
 function TeamController() {
-    
+    // Llama.get('../app/data/team.json').then( ( data ) => {
+    //     let container = document.getElementById('general-container');
+    //     container.innerHTML = '';
+
+    //     //0 = Director, 1 = coordinardor, 2 = capitan
+    //     let directores = [];
+    //     let coordinadores = [];
+    //     let capitanes = [];
+    //     let directoresInsert = '';
+    //     let coordinadoresInsert = '';
+    //     let capitanInsert = '';
+
+    //     data.forEach( d => {
+    //         switch (d.idPosition) {
+    //             case 0:
+    //                 directores.push(`
+    //                     <div class="img-team-container">
+    //                         <div class="img-team">
+    //                             <img src="${d.image}" alt="${d.name}">
+    //                         </div>
+    //                         <h2>${d.name}</h2>
+    //                     </div>
+    //                     `);
+    //                 break;
+    //             case 1:
+    //                 coordinadores.push(`
+    //                 <div class="img-team-container">
+    //                     <div class="img-team">
+    //                         <img src="${d.image}" alt="${d.name}">
+    //                     </div>
+    //                     <h2>${d.name}</h2>
+    //                 </div>
+    //                 `);
+    //                 break;
+    //             case 2:
+    //                 capitanes.push(`
+    //                 <div class="img-team-container">
+    //                     <div class="img-team">
+    //                         <img src="${d.image}" alt="${d.name}">
+    //                     </div>
+    //                     <h2>${d.name}</h2>
+    //                 </div>
+    //                 `);
+    //                 break;
+            
+    //             default:
+    //                 break;
+    //         }
+    //     });
+
+    //     directores.forEach( d => {
+    //         directores
+    //     });
+    //     coordinadores.forEach( c => {
+
+    //     });
+    //     capitanes.forEach( c => {
+
+    //     });
+
+    // }).catch( ( error ) => 
+    // {
+    //     console.log( error );
+    // });
 };
