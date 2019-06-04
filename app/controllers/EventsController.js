cf030215fcee2e1f30600e7b964fc929e0523f0b
Ignacio Donoso <naxonicolas1997@gmail.com>
2019-06-04 18:46:31 -0400

    Pagina eventos

--- app/controllers/EventsController.js
+++ app/controllers/EventsController.js
@@ -1,3 +1,26 @@
 function EventsController() {
-
+    let container = document.getElementById('general-container');
+    Llama.get('../app/data/events.json').then( ( data ) => {
+        console.log(data);
+        data.forEach( e => {
+            console.log(e.image);
+            container.innerHTML += 
+            `
+            <div class="event-container">
+                <div class="info-event">
+                    <h2>${e.name}</h2>
+                    <span id="decorator-title"></span>
+                </div>
+                <a data-lightbox="item-img-2" class="img-event" href="${e.image}">
+                    <img src="${e.image}" alt="${e.name}">
+                </a>
+                <div class="info-event">
+                    <p>${e.description}</p>
+                    <p>La fecha es${e.date} a las ${e.time}</p>
+                    <p></p>
+                </div>
+            </div>
+            `;
+        });
+    });
 };
--- app/services/Llama.js
+++ app/services/Llama.js
@@ -1,230 +1,296 @@
+"use strict";
+
 // Firefox 1.0+
-const isFirefox = typeof InstallTrigger !== 'undefined';
+var isFirefox = typeof InstallTrigger !== 'undefined';
 var SideNavOpen = false;
 
-const Llama = ( function( ) {
-    const paramsParse = function ( params ) {
-        let output = "";
-          let lenght = params.lenght;
-          for( let i in params ) {
-            output += i + "=" + params[ i ];
-            if( i < lenght - 1 ) {
-                output += "&";
-            }
-          }
-        return output;
+var Llama = function () {
+  var paramsParse = function paramsParse(params) {
+    var output = "",
+        counter = 0;
+
+    for (var i in params) {
+      output += i + "=" + params[i];
+
+      if (counter < Object.keys(params).length - 1) {
+        output += "&";
+      }
+
+      counter++;
     }
 
-    const post = function( url, params, encodingType ) {
-      const FILES = 1, DEFAULT = 0;  
-      const Encondings = [
-          "application/x-www-form-urlencoded",
-          "multipart/form-data",
-          "text/plain"
-        ];
-        let _encondingIndex = 0;
-        if( encodingType ) _encondingIndex = encodingType;
-        // Return a new promise.
-        return new Promise( function( resolve, reject ) {
-          // Do the usual XHR stuff
-          var req = new XMLHttpRequest(  );
-          req.open( 'POST', url, true );
-    
-          // The autorization
-          try{
-            if( localStorage.getItem( "token" ) ){
-                let token = JSON.parse( localStorage.getItem( "token" ) ).token;
-                req.setRequestHeader('Authorization', token );
-            }
-          } catch( error) {
-            reject( error )
-          }
-          
-          req.onload = function(  ) {
-            // This is called even on 404 etc
-            // so check the status
-            if ( req.status == 200 ) {
-              // Resolve the promise with the response text
-              let message  = "";
-                try {
-                    message = JSON.parse( req.response );
-                } catch (error) {
-                    message = req.response;
-                }
-              resolve( message );
-            }
-            else {
-                // Otherwise reject with the status text
-                // which will hopefully be a meaningful error
-                let message  = "";
-                try {
-                    message = JSON.parse( req.response );
-                } catch (error) {
-                    message = req.response;
-                }
-                reject( { status: req.status, message: Error( message )} );
-            }
-          };
-      
-          // Handle network errors
-          req.onerror = function(  ) {
-            reject( Error( "No es posible conectarse con el servidor, verifique su conexión a internet" ) );
-          };
-          
-          if( _encondingIndex == FILES ) {
-            var formData = new FormData();
-            for( let i in params ) {
-              formData.append( i, params[ i ] );
-            }
-            req.send( formData );
-          } else {
-            // Parse the data 
-            params = paramsParse( params );
-            //Send the proper header information along with the request
-            req.setRequestHeader('Content-type', Encondings[ DEFAULT ]);
-            // Make the request
-            req.send( params );
-          }
-        } );
-    }
+    return output;
+  };
 
-    const get = function( url ) {
-      // Return a new promise.
-      return new Promise( function( resolve, reject ) {
-        // Do the usual XHR stuff
-        var req = new XMLHttpRequest(  );
-        req.open( 'GET', url );
+  var post = function post(url, params, encodingType) {
+    var FILES = 1,
+        DEFAULT = 0;
+    var Encondings = ["application/x-www-form-urlencoded", "multipart/form-data", "text/plain"];
+    var _encondingIndex = 0;
+    if (encodingType) _encondingIndex = encodingType; // Return a new promise.
 
-        req.onload = function(  ) {
-          // This is called even on 404 etc
-          // so check the status
-          if ( req.status == 200 ) {
-            // Resolve the promise with the response text
-            let message  = "";
-                try {
-                    message = JSON.parse( req.response );
-                } catch (error) {
-                    message = req.response;
-                }
-              resolve( message );
-          }
-          else {
-              // Otherwise reject with the status text
-              // which will hopefully be a meaningful error
-              let message  = "";
-              try {
-                  message = JSON.parse( req.response ).message;
-              } catch (error) {
-                  message = req.response;
-              }
-              reject( { status: req.status, message: Error( message )} );
-          }
-        };
-    
-        // Handle network errors
-        req.onerror = function(  ) {
-          reject( Error( "No es posible conectarse con el servidor, verifique su conexión a internet" ) );
-        };
-        
-        // Make the request
-        req.send( );
-      } );
-    }
-  
-    return {
-        post: post,
-        get: get
-    };
-} ) ( );
+    return new Promise(function (resolve, reject) {
+      // Do the usual XHR stuff
+      var req = new XMLHttpRequest();
+      req.open('POST', url, true); // The autorization
 
-Llama.changePage = function( page ) {
-  if( page ) {
-    let main = document.getElementsByTagName( "main" ) [ 0 ];
-    Llama.get( Llama.pages[ page ].template ).then( ( result ) => {
+      try {
+        if (localStorage.getItem("token")) {
+          var token = JSON.parse(localStorage.getItem("token")).token;
+          req.setRequestHeader('Authorization', token);
+        }
+      } catch (error) {
+        reject(error);
+      }
+
+      req.onload = function () {
+        // This is called even on 404 etc
+        // so check the status
+        if (req.status == 200) {
+          // Resolve the promise with the response text
+          var message = "";
+
+          try {
+            message = JSON.parse(req.response);
+          } catch (error) {
+            message = req.response;
+          }
+
+          resolve(message);
+        } else {
+          // Otherwise reject with the status text
+          // which will hopefully be a meaningful error
+          var _message = "";
+
+          try {
+            _message = JSON.parse(req.response);
+          } catch (error) {
+            _message = req.response;
+          }
+
+          reject({
+            status: req.status,
+            message: Error(_message)
+          });
+        }
+      }; // Handle network errors
+
+
+      req.onerror = function () {
+        reject(Error("No es posible conectarse con el servidor, verifique su conexión a internet"));
+      };
+
+      if (_encondingIndex == FILES) {
+        var formData = new FormData();
+
+        for (var i in params) {
+          formData.append(i, params[i]);
+        }
+
+        req.send(formData);
+      } else {
+        // Parse the data 
+        params = paramsParse(params); //Send the proper header information along with the request
+
+        req.setRequestHeader('Content-type', Encondings[DEFAULT]); // Make the request
+
+        req.send(params);
+      }
+    });
+  };
+
+  var get = function get(url) {
+    // Return a new promise.
+    return new Promise(function (resolve, reject) {
+      // Do the usual XHR stuff
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+
+      req.onload = function () {
+        // This is called even on 404 etc
+        // so check the status
+        if (req.status == 200) {
+          // Resolve the promise with the response text
+          var message = "";
+
+          try {
+            message = JSON.parse(req.response);
+          } catch (error) {
+            message = req.response;
+          }
+
+          resolve(message);
+        } else {
+          // Otherwise reject with the status text
+          // which will hopefully be a meaningful error
+          var _message2 = "";
+
+          try {
+            _message2 = JSON.parse(req.response).message;
+          } catch (error) {
+            _message2 = req.response;
+          }
+
+          reject({
+            status: req.status,
+            message: Error(_message2)
+          });
+        }
+      }; // Handle network errors
+
+
+      req.onerror = function () {
+        reject(Error("No es posible conectarse con el servidor, verifique su conexión a internet"));
+      }; // Make the request
+
+
+      req.send();
+    });
+  };
+
+  return {
+    post: post,
+    get: get
+  };
+}();
+
+Llama.changePage = function (page) {
+  if (page) {
+    var main = document.getElementsByTagName("main")[0];
+    Llama.get(Llama.pages[page].template).then(function (result) {
       // Change the URL
       parent.location.hash = page;
-      main.innerHTML =  result; 
+      main.innerHTML = result;
       window.scrollTo(0, 0);
-      if( Llama.pages[ page ].controller ) {
-        Llama.pages[ page ].controller(  );
+
+      if (Llama.pages[page].controller) {
+        Llama.pages[page].controller();
       }
-    } ).catch( ( error ) => {
-      main.innerHTML =  error;
-    } );
-  } 
-}
-
-Llama.init = function( pages ) {
-    let optionMenus = document.getElementsByClassName( "menu" );
-    Llama.pages = pages;
-    for( let optionMenu of optionMenus ) {
-      optionMenu.addEventListener( "click", function( e ) {
-        e.preventDefault();
-        let path = optionMenu.getAttribute( "href" );
-        Llama.changePage( path );
-        closeNav( );
-      } );
-    }
-}
-
-Llama.setDefault = function( page ) {
-  Llama.homePage = page;
-  let url = parent.location.hash.split( '#' )[ 1 ];
-  if( !url ) {
-    Llama.changePage( page );
-  } else {
-    Llama.changePage( url );
+    }).catch(function (error) {
+      main.innerHTML = error;
+    });
   }
-  Llama.reloadMenu();
-}
+};
 
-window.onhashchange = function( ) { 
-  Llama.setDefault( Llama.homePage );
+Llama.init = function (pages) {
+  var optionMenus = document.getElementsByClassName("menu");
+  Llama.pages = pages;
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    var _loop = function _loop() {
+      var optionMenu = _step.value;
+      optionMenu.addEventListener("click", function (e) {
+        e.preventDefault();
+        var path = optionMenu.getAttribute("href");
+        Llama.changePage(path);
+        closeNav();
+      });
+    };
+
+    for (var _iterator = optionMenus[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      _loop();
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return != null) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+};
+
+Llama.setDefault = function (page) {
+  Llama.homePage = page;
+  var url = parent.location.hash.split('#')[1];
+
+  if (!url) {
+    Llama.changePage(page);
+  } else {
+    Llama.changePage(url);
+  }
+
+  Llama.reloadMenu();
+};
+
+window.onhashchange = function () {
+  Llama.setDefault(Llama.homePage);
   closeNav();
   Llama.reloadMenu();
-}
+};
 
-Llama.reloadMenu = function() {
-  let opts = document.getElementsByClassName( "opt" );
-  let url = parent.location.hash.split( '#' )[ 1 ];
-  for( let opt of opts ) {
-      if( opt.getAttribute( "href" ) == url ) {
-          opt.className += " active";
-          
+Llama.reloadMenu = function () {
+  var opts = document.getElementsByClassName("opt");
+  var url = parent.location.hash.split('#')[1];
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
+
+  try {
+    for (var _iterator2 = opts[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var opt = _step2.value;
+
+      if (opt.getAttribute("href") == url) {
+        opt.className += " active";
       } else {
-          opt.className = opt.className.replace( " active", "" );
+        opt.className = opt.className.replace(" active", "");
       }
+    }
+  } catch (err) {
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion2 && _iterator2.return != null) {
+        _iterator2.return();
+      }
+    } finally {
+      if (_didIteratorError2) {
+        throw _iteratorError2;
+      }
+    }
   }
-}
+};
 
 function btnSideNav() {
-  if( SideNavOpen ){
+  if (SideNavOpen) {
     closeNav();
-  }  else {
-    openNav( )
+  } else {
+    openNav();
   }
 }
 
 closeNav();
 
 function openNav() {
-  let sideNav = document.getElementById("sidenav")
-  if( sideNav ) {
+  var sideNav = document.getElementById("sidenav");
+
+  if (sideNav) {
     // sideNav.style.width = "250px";
     sideNav.className = "sidenav";
-    let menubars = document.getElementById( "menuBars" );
+    var menubars = document.getElementById("menuBars");
     menubars.className = "far fa-window-close bars";
     SideNavOpen = true;
   }
 }
 
 function closeNav() {
-  let sideNav = document.getElementById("sidenav");
-  if( sideNav ) {
-      sideNav.className = "hsidenav";
-      // document.getElementById("sidenav").style.width = "0";
-      SideNavOpen = false;
-      let menubars = document.getElementById( "menuBars" );
-      menubars.className = "fas fa-bars bars";
+  var sideNav = document.getElementById("sidenav");
+
+  if (sideNav) {
+    sideNav.className = "hsidenav"; // document.getElementById("sidenav").style.width = "0";
+
+    SideNavOpen = false;
+    var menubars = document.getElementById("menuBars");
+    menubars.className = "fas fa-bars bars";
   }
 }
